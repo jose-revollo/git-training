806178efb2ce0b837b178aa9ce60366329d004b1
jose-revollo <jose.revollo@protemo.com>
2014-08-05 14:55:58 -0400

    add changes for new_task

--- src/main/java/gitTra/Hello.java
+++ src/main/java/gitTra/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world, lest goo!!");
+		System.out.println("cambios git local, en branch new_task");
 	}
 
 }
